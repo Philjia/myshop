d25fcf8ca4bbacb6894f293256ad8629e9b62753
Phil Jia <philjia@Phils-iMac.modem>
2020-11-17 00:15:33 +1100

    master

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7a95796d7e7d56d49b94393eb680b2cf'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ac96cbc1-b0e0-47a4-9eb6-2ad451348933'
+};
